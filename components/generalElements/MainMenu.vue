4a10d78850fe17f002bd9f15eccc4f035052e617
ry-jp <ry_design@protonmail.com>
2023-06-02 12:15:39 -0500

    first commit, small design changes
    small design changes for prototype

--- components/generalElements/MainMenu.vue
+++ components/generalElements/MainMenu.vue
@@ -1,8 +1,10 @@
 <template>
   <nav>
     <NuxtLink to="/">{{ $t('messages.general.home') }}</NuxtLink>
-    <NuxtLink to="/products">{{ $t('messages.general.allProducts') }}</NuxtLink>
+    <NuxtLink to="/products">Product Catalog</NuxtLink>
     <NuxtLink to="/categories">{{ $t('messages.shop.category', 2) }}</NuxtLink>
+    <NuxtLink to="/contact">About Us</NuxtLink>
     <NuxtLink to="/contact">{{ $t('messages.general.contact') }}</NuxtLink>
+    <NuxtLink to="/contact">{{ $t('Account Login') }}</NuxtLink>
   </nav>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -33,35 +33,5 @@ useHead({
       </div>
     </section>
 
-    <section class="container grid gap-4 my-24 lg:grid-cols-4">
-      <div class="flex items-center gap-8 p-8 bg-white rounded-lg">
-        <img src="/icons/box.svg" width="60" height="60" alt="Free Shipping" loading="lazy" />
-        <div>
-          <h3 class="text-xl font-semibold">Free Shipping</h3>
-          <p class="text-sm">Free shipping on order over €50</p>
-        </div>
-      </div>
-      <div class="flex items-center gap-8 p-8 bg-white rounded-lg">
-        <img src="/icons/moneyback.svg" width="60" height="60" alt="Money Back" loading="lazy" />
-        <div>
-          <h3 class="text-xl font-semibold">Peace of Mind</h3>
-          <p class="text-sm">30 days money back guarantee</p>
-        </div>
-      </div>
-      <div class="flex items-center gap-8 p-8 bg-white rounded-lg">
-        <img src="/icons/secure.svg" width="60" height="60" alt="Secure Payment" loading="lazy" />
-        <div>
-          <h3 class="text-xl font-semibold">100% Payment Secure</h3>
-          <p class="text-sm">Your payment are safe with us.</p>
-        </div>
-      </div>
-      <div class="flex items-center gap-8 p-8 bg-white rounded-lg">
-        <img src="/icons/support.svg" width="60" height="60" alt="Support 24/7" loading="lazy" />
-        <div>
-          <h3 class="text-xl font-semibold">Support 24/7</h3>
-          <p class="text-sm">24/7 Online support</p>
-        </div>
-      </div>
-    </section>
   </main>
 </template>
